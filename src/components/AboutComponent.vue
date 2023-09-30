4d0232cd88bc1679b2e8f5d5d0f34e1fb369be89
Yeferson Rosales <yefersonj.rr@gmail.com>
2023-09-29 23:46:01 -0300

    se cambia una letra

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h3>Este es una pagina de about desde la rama frontend de yeferson</h3>
+        <h3>Este es una pagina de about desde la rama frontend de yeferson.</h3>
     </div>
 </template>
