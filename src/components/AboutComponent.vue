b60f7a3ff3c13c1978172ddfd31b60aafce43ce8
Yeferson Rosales <yefersonj.rr@gmail.com>
2023-09-29 23:41:24 -0300

    se actualiza rama de prueba, yeferson

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h3>Este es una pagina de about desde la rama frontend de yeferson</h3>
+    </div>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AboutComponent/>
   </div>
 </template>
+<script>
+import AboutComponent from '@/components/AboutComponent.vue'
+export default{
+  name: 'AboutView',
+  components:{
+    AboutComponent
+  }
+}
+</script>
