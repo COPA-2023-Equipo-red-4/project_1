28f6df16a8abaf622c2625ab7dc0a77126691278
LUIS-TORRELLAS <luistorrellas.col@gmail.com>
2023-10-01 18:46:10 -0500

    Se agrega componente del body

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <PrincipalComponent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import PrincipalComponent from '@/components/PrincipalComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    PrincipalComponent
   }
 }
 </script>
