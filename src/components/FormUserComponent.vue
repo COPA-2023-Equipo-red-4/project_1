3bb51d6f42d4ed2368096bf890a5438617417924
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-11-21 01:19:31 -0300

    Update FormUserComponent.vue

--- src/components/FormUserComponent.vue
+++ src/components/FormUserComponent.vue
@@ -14,17 +14,13 @@
                                     <div class="form-floating mb-3">
                                         <input type="text" class="form-control" id="nombres" v-model="nombres"
                                             placeholder="nombre">
-                                        placeholder="nombre">
                                         <label for="floatingInput">Nombre</label>
                                     </div>
-
                                     <div class="form-floating mb-3">
                                         <input type="text" class="form-control" id="apellidos" placeholder="apellido"
                                             v-model="apellido">
-                                        v-model="apellido">
                                         <label for="floatingInput">Apellido</label>
                                     </div>
-
                                     <div class="form-floating mb-3">
                                         <input type="text" class="form-control" id="telefono" placeholder="telefono"
                                             v-model="telefono">
@@ -34,63 +30,51 @@
                                     <div class="form-floating mb-3">
                                         <input type="date" class="form-control" id="fecha_nacimiento"
                                             placeholder="fechaDeNacimiento" v-model="fecha_nacimiento">
-                                        placeholder="fechaDeNacimiento" v-model="fecha_nacimiento">
                                         <label for="floatingInput">Fecha de nacimiento</label>
                                     </div>
 
                                     <div class="form-floating mb-3">
                                         <input type="text" class="form-control" id="domicilio" placeholder="domicilio"
                                             v-model="domicilio">
-                                        v-model="domicilio">
+
                                         <label for="floatingInput">Domicilio</label>
                                     </div>
-
                                     <div class="form-floating mb-3">
                                         <input type="text" class="form-control" id="ciudad" placeholder="ciudad"
                                             v-model="ciudad">
-                                        v-model="ciudad">
                                         <label for="floatingInput">Ciudad</label>
                                     </div>
-
                                     <div class="form-floating mb-3">
-                                        <input type="text" class="form-control" id="pais" placeholder="pais" v-model="pais">
                                         <input type="text" class="form-control" id="pais" placeholder="pais" v-model="pais">
                                         <label for="floatingInput">pais</label>
                                     </div>
-
                                     <h4>Indica tu sexo</h4>
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" v-model="sexo" value="M">
-                                        <input class="form-check-input" type="radio" v-model="sexo" value="M">
                                         <label class="form-check-label" for="flexRadioDefault1">
                                             Masculino
                                         </label>
                                     </div>
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" v-model="sexo" value="F">
-                                        <input class="form-check-input" type="radio" v-model="sexo" value="F">
                                         <label class="form-check-label" for="flexRadioDefault2">
                                             Femenino
                                         </label>
                                     </div>
-
                                     <h4>Indica tu Género</h4>
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" v-model="genero" value="M">
-                                        <input class="form-check-input" type="radio" v-model="genero" value="M">
                                         <label class="form-check-label" for="flexRadioDefault1">
                                             Masculino
                                         </label>
                                     </div>
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" v-model="genero" value="F">
-                                        <input class="form-check-input" type="radio" v-model="genero" value="F">
                                         <label class="form-check-label" for="flexRadioDefault2">
                                             Femenino
                                         </label>
                                     </div>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" v-model="genero" value="X">
                                         <input class="form-check-input" type="radio" v-model="genero" value="X">
                                         <label class="form-check-label" for="flexRadioDefault2">
                                             Otros
@@ -100,18 +84,14 @@
                                     <div class="form-floating mb-3">
                                         <input type="email" class="form-control" id="email" placeholder="name@example.com"
                                             v-model="email">
-                                        v-model="email">
                                         <label for="floatingInput">Email</label>
                                     </div>
-
                                     <div class="mb-3">
                                         <label for="formFile" class="form-label">Foto</label>
-                                        <input class="form-control" type="file" @change="handleFileChange">
                                         <input class="form-control" type="file" @change="handleFileChange">
                                     </div>
 
                                     <div class="form-floating mb-3">
-                                        <input type="password" class="form-control" placeholder="Contraseña" v-model="pass">
                                         <input type="password" class="form-control" placeholder="Contraseña" v-model="pass">
                                         <label for="floatingPassword">Contraseña</label>
                                     </div>
@@ -119,22 +99,17 @@
                                     <div class="form-floating mb-3">
                                         <input type="password" class="form-control" placeholder="confirmar contraseña"
                                             v-model="confirmPassword">
-                                        <input type="password" class="form-control" placeholder="confirmar contraseña"
-                                            v-model="confirmPassword">
                                         <label for="floatingPassword">Confirmar contraseña</label>
                                     </div>
-
                                     <div class="form-check mb-3">
                                         <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
                                         <label class="form-check-label" for="rememberPasswordCheck">
                                             Recuerda contraseña
                                         </label>
                                     </div>
-
                                     <div class="d-grid">
                                         <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2"
                                             type="button" @click="submitForm"> Registrate </button>
-                                        type="button" @click="submitForm"> Registrate </button>
                                         <div class="text-center">
                                             <a class="small" href="#">Olvidaste tu contraseña?</a>
                                         </div>
@@ -177,6 +152,7 @@ export default {
             console.log('en submitform');
             const formdata = new FormData();
             formdata.append("nombres", this.nombres);
+            formdata.append("apellido", this.apellido);
             formdata.append("pass", this.pass);
             formdata.append("email", this.email);
             formdata.append("telefono", this.telefono);
@@ -201,7 +177,7 @@ export default {
                     console.log('Cookies:', cookies);
 
                     if (response.ok) {
-                        return response.text();
+                        return response.json();
                     } else {
                         throw new Error('Error NO PUDIMOA CREAR EL USUARIO ');
                     }
@@ -214,7 +190,6 @@ export default {
                 })
                 .catch(error => console.error('Error:', error));
         },
-
         handleFileChange(event) {
             this.foto = event.target.files[0];
         },
